fdf7ebfbb3fc8fcfed452f98426605d39158c202
weijinyang <wjy971208@163.com>
2020-02-06 12:51:55 +0800

    ls -l | wc -l

---
+++ pipe0205/pipe2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char* argv[])
+{
+    int fd[2];
+    int pi=pipe(fd);
+    if(pi<0)
+    {
+	perror("pipe error");
+	exit(1);
+    }
+    pid_t pid=fork();
+    if(pid==0)
+    {
+	close(fd[0]);	
+        dup2(fd[1],STDOUT_FILENO);
+        close(fd[1]);
+        execlp("ls","ls","-l",NULL);
+        exit(1);
+    }
+    else if(pid<0)
+    {
+	perror("fork error");
+	exit(1);
+    }
+    else
+    {
+	close(fd[1]);
+	dup2(fd[0],STDIN_FILENO);
+	close(fd[0]);
+	execlp("wc","wc","-l",NULL);
+	exit(1);
+    }
+    return 0;
+}
